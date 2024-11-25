5d0c064470ef0288bd903cb30761f3e576aee325
Sawitt Ngamvilaisiriwong <sawittngam2@gmail.com>
2024-11-25 13:07:53 +0700

    Add title and label graph

--- app.py
+++ app.py
@@ -31,8 +31,16 @@ def plot_raw_data(data, show_open, show_close):
         fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name='Stock Open', line=dict(color='lightgreen')))
     if show_close:
         fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name='Stock Close', line=dict(color='red')))
+
     fig.update_layout(
-        title_text=TICKER.upper(),
+        title=dict(
+            text="Stock Close and Stock Open of " + TICKER.upper(),
+            x=0.5,  # Centers the title
+            xanchor='center',  # Ensures alignment to the center
+            yanchor='top'
+        ),
+        xaxis_title="Date",  # Title for the x-axis
+        yaxis_title="Stock Price",  # Title for the y-axis
         xaxis_rangeslider_visible=False,
         width=900,
         height=600,
@@ -40,8 +48,9 @@ def plot_raw_data(data, show_open, show_close):
         paper_bgcolor='rgba(0, 0, 0, 0)',  # Transparent background for the entire figure
         font=dict(color='white')  # Text color can be adjusted for visibility
     )
-    fig.write_html("D:/xampp/htdocs/asset/graph/raw_data_plot.html")
+    fig.write_html("C:/xampp/htdocs/asset/graph/raw_data_plot.html")
     print("Raw data plot saved as 'raw_data_plot.html'.")
+
 
 plot_raw_data(data, show_open, show_close)
 
@@ -72,5 +81,5 @@ fig1.update_layout(
     font=dict(color='black')  # Set text color to white
 )
 
-fig1.write_html("D:/xampp/htdocs/asset/graph/forecast_plot.html")
+fig1.write_html("C:/xampp/htdocs/asset/graph/forecast_plot.html")
 print("Forecast plot saved as 'forecast_plot.html'.")
