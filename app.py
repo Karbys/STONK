238bf053c81089676c04cd87b5ad49bce7d20956
Emma Paris <eparis@gmail.com>
2024-11-26 21:14:34 +0700

    Change A lot

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ TODAY = date.today().strftime("%Y-%m-%d")
 TICKER = sys.argv[1] if len(sys.argv) > 1 else "AAPL"
 n_years = int(sys.argv[2]) if len(sys.argv) > 2 else 1
 show_open = bool(int(sys.argv[3])) if len(sys.argv) > 3 else False  # Convert "1"/"0" to True/False
-show_close = bool(int(sys.argv[4])) if len(sys.argv) > 4 else False  # Convert "1"/"0" to True/False
 period = n_years * 365
 
 # Load data
@@ -24,26 +23,90 @@ def load_data(ticker):
 
 data = load_data(TICKER)
 
-# Plot raw data based on checkbox selections
-def plot_raw_data(data, show_open, show_close):
+# Function to create an empty graph
+def plot_empty_graph(title, file_name):
     fig = go.Figure()
-    if show_open:
-        fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name='Stock Open', line=dict(color='lightgreen')))
-    if show_close:
-        fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name='Stock Close', line=dict(color='red')))
     fig.update_layout(
-        title_text=TICKER.upper(),
-        xaxis_rangeslider_visible=False,
+        title_text=title,
+        xaxis=dict(title='Date', range=['2012-01-01', TODAY]),
+        yaxis=dict(title='Value', range=[0, 1]),
         width=900,
         height=600,
-        plot_bgcolor='rgba(0, 0, 0, 0)',  # Transparent background for the plot area
-        paper_bgcolor='rgba(0, 0, 0, 0)',  # Transparent background for the entire figure
-        font=dict(color='white')  # Text color can be adjusted for visibility
+        plot_bgcolor='rgba(0, 0, 0, 0)',  # Transparent background
+        paper_bgcolor='rgba(0, 0, 0, 0)',  # Transparent background
+        font=dict(color='white')  # Text color
     )
-    fig.write_html("D:/xampp/htdocs/asset/graph/raw_data_plot.html")
-    print("Raw data plot saved as 'raw_data_plot.html'.")
+    fig.write_html(file_name)
+    print(f"Empty graph saved as '{file_name}'.")
 
-plot_raw_data(data, show_open, show_close)
+# Check if data is available
+if data.empty:
+    print(f"No data available for ticker '{TICKER}'. Plotting empty graphs.")
+    plot_empty_graph(f"{TICKER.upper()} Raw Data (No Data Available)", "D:/xampp/htdocs/asset/graph/raw_data_plot.html")
+    plot_empty_graph(f"{TICKER.upper()} Forecast (No Data Available)", "D:/xampp/htdocs/asset/graph/forecast_plot.html")
+else:
+    # Plot raw data
+    def plot_raw_data(data):
+        fig = go.Figure()
+
+        # Add line for Close price with filled area under the line
+        fig.add_trace(go.Scatter(
+            x=data['Date'],
+            y=data['Close'],
+            mode='lines',
+            name='<b>Date</b>',
+            line=dict(color='lightgreen'),
+            fill='tozeroy',  # Fill the area below the line with the same color
+            fillcolor='rgba(144, 238, 144, 0.3)',  # Light green with some transparency
+            hovertemplate=(
+                "%{x|%d %B %Y}<br>"  # Format: Day Month Year (e.g., 01 January 2024)
+                "<b>Close:</b> %{y:.2f} USD<br>"
+                "<b>Open:</b> %{customdata[0]:.2f} USD<br>"
+                "<b>High:</b> %{customdata[1]:.2f} USD<br>"
+                "<b>Low:</b> %{customdata[2]:.2f} USD<br>"
+                "<b>Volume:</b> %{customdata[3]:,}<br>"
+            ),
+            customdata=data[['Open', 'High', 'Low', 'Volume']].values,
+            hoverlabel=dict(
+                bgcolor="black",  # Set background color to black
+                font=dict(color="white")  # Set text color to white
+            )
+        ))
+
+        # Customize layout
+        fig.update_layout(
+            title_text=f"{TICKER.upper()} Stock Prices",
+            xaxis_title="Date",
+            yaxis_title="Price (USD)",
+            template="plotly_dark",
+            hovermode="x unified",  # Hover over the entire line
+            width=900,
+            height=600,
+            plot_bgcolor="rgba(0, 0, 0, 0)",  # Transparent background
+            paper_bgcolor="rgba(0, 0, 0, 0)",  # Transparent background
+            font=dict(color="white"),  # Text color
+            hoverlabel=dict(
+                bgcolor="black",  # Set background color to black
+                font=dict(color="white")  # Set text color to white
+            ),
+            xaxis=dict(
+                rangeslider=dict(visible=False),  # Add a range slider for interactive zooming
+                showgrid=True,  # Enable grid for better readability
+                zeroline=False,  # Remove the x-axis zero line
+            ),
+            yaxis=dict(
+                showgrid=True,  # Enable grid for better readability
+                zeroline=False,  # Remove the y-axis zero line
+            )
+        )
+
+
+        # Save the chart
+        fig.write_html("D:/xampp/htdocs/asset/graph/raw_data_plot.html")
+        print("Raw data plot saved as 'raw_data_plot.html'.")
+
+# Call the function to plot raw data
+plot_raw_data(data)
 
 # Prepare data for Prophet
 df_train = data[['Date', 'Close']]
@@ -58,19 +121,64 @@ forecast = m.predict(future)
 # Save forecast plot
 fig1 = plot_plotly(m, forecast)
 
-# Modify forecast plot to set marker color to white for the data points
-fig1.update_traces(
-    marker=dict(color='white', size=5)  # Set marker color to white and size to 5 (you can adjust the size)
-)
+# Add line for Close price with filled area under the line
+fig1.add_trace(go.Scatter(
+    x=data['Date'],
+    y=data['Close'],
+    mode='lines',
+    name='<b>Date</b>',
+    line=dict(color='orange'),
+    fill='tozeroy',  # Fill the area below the line with the same color
+    fillcolor='rgba(255, 165, 0, 0.2)',  # Light green with some transparency
+    hovertemplate=(
+        "%{x|%d %B %Y}<br>"  # Format: Day Month Year (e.g., 01 January 2024)
+        "<b>Close:</b> %{y:.2f} USD<br>"
+        "<b>Open:</b> %{customdata[0]:.2f} USD<br>"
+        "<b>High:</b> %{customdata[1]:.2f} USD<br>"
+        "<b>Low:</b> %{customdata[2]:.2f} USD<br>"
+        "<b>Volume:</b> %{customdata[3]:,}<br>"
+    ),
+    customdata=data[['Open', 'High', 'Low', 'Volume']].values,
+    
+))
 
+# Add actual data points (historical data) as markers
+# fig1.add_trace(go.Scatter(
+#     x=df_train['ds'],
+#     y=df_train['y'],
+#     mode='markers',
+#     marker=dict(color='white', size=3, line=dict(color='black', width=1)),  # Red points with black border
+#     name='Actual Data'
+# ))
+
+# Update layout settings
 fig1.update_layout(
-    xaxis_rangeslider_visible=False,
+    title_text=f"{TICKER.upper()} Stock Price Forecast",
+    xaxis_title="Date",
+    yaxis_title="Price (USD)",
+    template="plotly_dark",
+    hovermode="x unified",  # Hover over the entire line
     width=900,
     height=600,
-    plot_bgcolor='rgba(0, 0, 0, 0)',  # Transparent background for the plot area
-    paper_bgcolor='rgba(0, 0, 0, 0)',  # Transparent background for the entire figure
-    font=dict(color='black')  # Set text color to white
+    plot_bgcolor="rgba(0, 0, 0, 0)",  # Transparent background
+    paper_bgcolor="rgba(0, 0, 0, 0)",  # Transparent background
+    font=dict(color="white"),  # Text color
+    hoverlabel=dict(
+        bgcolor="black",  # Set background color to black
+        font=dict(color="white")  # Set text color to white
+    ),
+    xaxis=dict(
+        rangeslider=dict(visible=False),  # Add a range slider for interactive zooming
+        rangeselector=dict(visible=False),  # Disable range selector for more clarity
+        showgrid=True,  # Enable grid for better readability
+        zeroline=False,  # Remove the x-axis zero line
+    ),
+    yaxis=dict(
+        showgrid=True,  # Enable grid for better readability
+        zeroline=False,  # Remove the y-axis zero line
+    )
 )
 
+# Save the forecast chart
 fig1.write_html("D:/xampp/htdocs/asset/graph/forecast_plot.html")
 print("Forecast plot saved as 'forecast_plot.html'.")
--- test/index.php
+++ test/index.php
@@ -7,40 +7,143 @@
     <link rel="stylesheet" href="../asset/css/test.css">
 </head>
 <body>
-    <div class="header">
-        <h1>Interactive Plot from Python</h1>
+    <div class="navbar">
+        <div class="nav-con">
+            <div class="logo">
+                <h1>S T O N K</h1>
+            </div>
+            <div class="menu">
+                <ul>
+                    <li><a href="../">Home</a></li>
+                    <li><a href="../#section_2">Member</a></li>
+                    <li><a href="../#about-con">About</a></li>
+                    <li><a href="/test/">Graph</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="header">
+            <h1>Interactive Plot from Python</h1>
+        </div>
+        <!-- Form to collect user inputs for the stock ticker and prediction period -->
+        <div class="form-con">
+            <form action="process.php" method="post">
+                <div class="ticker-con">
+                    <!-- <label for="ticker">Enter stock ticker (e.g., AAPL) </label> -->
+                    <br>
+                    <input type="text" id="ticker" name="ticker" placeholder="Enter stock ticker(e.g., AAPL)" required>
+                </div>
+                <div class="year-con">
+                    <label for="years">Year (1 to 4)</label>
+                    <br>
+                    <!-- <input type="range" id="years" name="years" min="1" max="4" class="slider" value="1"required> -->
+                    <span id="rangeValue">0</span>
+                    <Input class="range" type="range" id="years" name="years" value="1" min="1" max="4" onChange="rangeSlide(this.value)" onmousemove="rangeSlide(this.value)" required></Input>
+                </div>
+
+                <!-- <div class="checkbox-con">
+                    <label>Select data to display:</label><br>
+                    <input type="checkbox" id="stock_open" name="stock_open" value="1" checked>
+                    <label for="stock_open">Stock Open</label><br>
+                    <input type="checkbox" id="stock_close" name="stock_close" value="1" checked>
+                    <label for="stock_close">Stock Close</label>
+                </div> -->
+
+                <!-- <div class="customCheckBoxHolder">
+                    <input class="customCheckBoxInput" id="stock_open" name="stock_open" type="checkbox" value="1" checked>
+                    <label class="customCheckBoxWrapper" for="stock_open">
+                        <div class="customCheckBox">
+                            <div class="inner">Stock Open</div>
+                        </div>
+                    </label>
+
+                    <input class="customCheckBoxInput" id="stock_close" name="stock_close" type="checkbox" value="1" checked>
+                    <label class="customCheckBoxWrapper" for="stock_close">
+                        <div class="customCheckBox">
+                            <div class="inner">Stock Close</div>
+                        </div>
+                    </label>
+                </div> -->
+
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+
+        <!-- Display the plots -->
+        <div class="frame-con">
+            <div class="frame-item">
+                <h2>Stock Data</h2>
+                <iframe src="../asset/graph/raw_data_plot.html" width="100%" height="620px" style="border: none;"></iframe>
+            </div>
+            <div class="frame-item">
+                <h2>Forecast Plot</h2>
+                <iframe src="../asset/graph/forecast_plot.html" width="100%" height="620px" style="border: none;"></iframe>
+            </div>
+        </div>
+
         <div class="button">
             <a href="../">Back</a>
         </div>
-    </div>
-    <!-- Form to collect user inputs for the stock ticker and prediction period -->
-    <div class="form-con">
-        <form action="process.php" method="post">
-            <div class="ticker-con">
-                <label for="ticker">Enter stock ticker (e.g., AAPL, GOOGL): </label>
-                <br>
-                <input type="text" id="ticker" name="ticker" required>
-            </div>
-            <div class="year-con">
-                <label for="years">Enter years of prediction (1 to 4): </label>
-                <br>
-                <input type="range" id="years" name="years" min="1" max="4" class="slider" value="1" required>
-            </div>
-            <div class="checkbox-con">
-                <label>Select data to display:</label><br>
-                <input type="checkbox" id="stock_open" name="stock_open" value="1" checked>
-                <label for="stock_open">Stock Open</label><br>
-                <input type="checkbox" id="stock_close" name="stock_close" value="1" checked>
-                <label for="stock_close">Stock Close</label>
-            </div>
-            <button type="submit">Submit</button>
-        </form>
+
+        <div class="stars">
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+        </div>
     </div>
 
-    <!-- Display the plots -->
-    <h2>Raw Data Plot</h2>
-    <iframe src="../asset/graph/raw_data_plot.html" width="50%" height="620px" style="border: none;"></iframe>
-    <h2>Forecast Plot</h2>
-    <iframe src="../asset/graph/forecast_plot.html" width="50%" height="620px" style="border: none;"></iframe>
+    <script type="text/javascript">
+        function rangeSlide(value) {
+            document.getElementById('rangeValue').innerHTML = value;
+        }
+    </script>
 </body>
 </html>
